b5739d31498fa6d33914273754bfaf1dc25cf7a0
eunbime <ebk1014@gmail.com>
2025-01-24 16:51:24 +0900

    feat: PWA 적용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-const nextConfig = {
+import withPWA from "next-pwa";
+
+const config = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})({
   images: {
     domains: ["utfs.io", "o4b06yr4hm.ufs.sh", "lh3.googleusercontent.com"],
   },
-};
+});
 
-export default nextConfig;
+export default config;
