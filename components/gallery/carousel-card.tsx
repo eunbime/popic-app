202324796f76ce27c53ceda88730cee21918cc7b
eunbime <ebk1014@gmail.com>
2025-01-25 21:21:10 +0900

    delete: format date 제거

--- components/gallery/carousel-card.tsx
+++ components/gallery/carousel-card.tsx
@@ -1,4 +1,3 @@
-import { format } from "date-fns";
 import Image from "next/image";
 
 interface CarouselCardProps {
@@ -9,12 +8,7 @@ interface CarouselCardProps {
   isSelected?: boolean;
 }
 
-const CarouselCard = ({
-  date,
-  onClick,
-  count,
-  thumbnailUrl,
-}: CarouselCardProps) => {
+const CarouselCard = ({ onClick, count, thumbnailUrl }: CarouselCardProps) => {
   return (
     <div
       className="relative h-full bg-gray-200 cursor-pointer"
@@ -32,9 +26,6 @@ const CarouselCard = ({
         )}
       </div>
       <div className="flex justify-center w-full h-full gap-1 bg-gray-200">
-        {/* <span className="absolute bottom-[3px] right-[2px] text-[8px] text-black font-semibold opacity-40 rounded-md px-1">
-          {format(date, "yyyy.MM.dd")}
-        </span> */}
         <span className="absolute flex items-center justify-center top-[3px] right-[2px] text-[8px] bg-black text-white w-4 h-4 text-center rounded-full opacity-70">
           {count}
         </span>
